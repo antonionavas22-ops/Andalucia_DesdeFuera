6a84210aff02e546d9303a8070ec3782543540c8
antonionavas22-ops <antonionavas_22@outlook.es>
2026-04-13 18:34:27 +0200

    Add files via upload

--- Calvert_1908.R
+++ Calvert_1908.R
@@ -1,3 +1,17 @@
-install.packages(c("usethis", "gitcreds"))
-usethis::use_git_config(user.name = "TuNombreDeUsuario", user.email = "tu@email.com")
-
+getwd()
+list.files()
+relato <- readLines("Calvert_1908.txt", encoding = "UTF-8")
+libro <- paste(relato, collapse = " ")
+libro_previo <- tolower(libro)
+install.packages("tidyverse")
+library("tidyverse")
+install.packages ("tidytext")
+library("tidytext")
+library("readxl")
+localidades<- read_excel("normalizacion_localidades.xlsx")
+View(localidades)
+resultado <- localidades %>%
+  mutate(menciones = str_count(libro_previo, fixed(english))) %>%
+  filter(menciones > 0) %>%
+  arrange(desc(menciones))
+View(resultado)
